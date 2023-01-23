03c298fb6b754ce768741bb54e1c71ee0c212a53
Allison Chapman <achapman@digitalocean.com>
2023-01-22 23:32:30 -0500

    Add search script

---
+++ us_state_abbrv.py
@@ -0,0 +1,76 @@
+# United States of America Python Dictionary to translate States,
+# Districts & Territories to Two-Letter codes and vice versa.
+#
+# Canonical URL: https://gist.github.com/rogerallen/1583593
+#
+# Dedicated to the public domain.  To the extent possible under law,
+# Roger Allen has waived all copyright and related or neighboring
+# rights to this code.  Data originally from Wikipedia at the url:
+# https://en.wikipedia.org/wiki/ISO_3166-2:US
+#
+# Automatically Generated 2021-09-11 18:04:36 via Jupyter Notebook from
+# https://gist.github.com/rogerallen/d75440e8e5ea4762374dfd5c1ddf84e0
+
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+    "US": "US"
+}
+
+# invert the dictionary
+abbrev_to_us_state = dict(map(reversed, us_state_to_abbrev.items()))
