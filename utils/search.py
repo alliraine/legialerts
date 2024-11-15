d236edd3ab0166593c193bb4afb20aea9195fc0d
Caleb <57644769+alliraine@users.noreply.github.com>
2024-11-15 09:46:19 -0500

    fix search for 2025

--- utils/search.py
+++ utils/search.py
@@ -60,15 +60,15 @@ def search(term, page, interactive):
                             prev_gsheet2["Number"] == bill["bill_number"]) & (
                                     prev_gsheet2["Summary"] == bill["title"]))]
 
-            # lscan3 = prev_gsheet3.loc[(prev_gsheet3["Bill ID"] == bill["bill_id"]) | (
-            #             (prev_gsheet3["State"] == abbrev_to_us_state[bill["state"]]) & (
-            #                 prev_gsheet3["Number"] == bill["bill_number"]) & (
-            #                         prev_gsheet3["Summary"] == bill["title"]))]
-            #
-            # lscan4 = prev_gsheet4.loc[(prev_gsheet4["Bill ID"] == bill["bill_id"]) | (
-            #             (prev_gsheet4["State"] == abbrev_to_us_state[bill["state"]]) & (
-            #                 prev_gsheet4["Number"] == bill["bill_number"]) & (
-            #                         prev_gsheet4["Summary"] == bill["title"]))]
+            lscan3 = prev_gsheet3.loc[(prev_gsheet3["Bill ID"] == bill["bill_id"]) | (
+                        (prev_gsheet3["State"] == abbrev_to_us_state[bill["state"]]) & (
+                            prev_gsheet3["Number"] == bill["bill_number"]) & (
+                                    prev_gsheet3["Summary"] == bill["title"]))]
+
+            lscan4 = prev_gsheet4.loc[(prev_gsheet4["Bill ID"] == bill["bill_id"]) | (
+                        (prev_gsheet4["State"] == abbrev_to_us_state[bill["state"]]) & (
+                            prev_gsheet4["Number"] == bill["bill_number"]) & (
+                                    prev_gsheet4["Summary"] == bill["title"]))]
 
             lscan5 = prev_gsheet5.loc[(prev_gsheet5["Bill ID"] == bill["bill_id"]) | (
                         (prev_gsheet5["State"] == abbrev_to_us_state[bill["state"]]) & (
@@ -77,15 +77,15 @@ def search(term, page, interactive):
 
             lscan6 = ignore_list.loc[(ignore_list["bill_id"] == bill["bill_id"])]
 
-            if lscan.empty and lscan2.empty and lscan5.empty and lscan6.empty:
+            if lscan.empty and lscan2.empty and lscan3.empty and lscan4.empty and lscan5.empty and lscan6.empty:
                 if bill["last_action_date"] is not None:
-                    if datetime.strptime(bill["last_action_date"], '%Y-%m-%d') == datetime(2024, 3, 8):
+                    if datetime.strptime(bill["last_action_date"], '%Y-%m-%d') == datetime(2024, 11, 15):
                         print(color.RED, bill["last_action_date"], abbrev_to_us_state[bill["state"]], bill["bill_number"],
                           bill["title"], bill["text_url"], color.END)
-                    elif datetime.strptime(bill["last_action_date"], '%Y-%m-%d') > datetime(2024, 3, 8):
+                    elif datetime.strptime(bill["last_action_date"], '%Y-%m-%d') > datetime(2024, 11, 15):
                         print(color.YELLOW, bill["last_action_date"], abbrev_to_us_state[bill["state"]], bill["bill_number"],
                               bill["title"], bill["text_url"], color.END)
-                    elif datetime.strptime(bill["last_action_date"], '%Y-%m-%d') > datetime(2024, 3, 6):
+                    elif datetime.strptime(bill["last_action_date"], '%Y-%m-%d') > datetime(2024, 11, 1):
                         print(bill["last_action_date"], abbrev_to_us_state[bill["state"]], bill["bill_number"],
                               bill["title"], bill["text_url"])
                 else:
@@ -121,21 +121,21 @@ expected_headers = wks.row_values(1)
 prev_gsheet = pd.DataFrame(wks.get_all_records(expected_headers=expected_headers))
 
 # open worksheet
-wks2 = gc.open_by_key(os.environ.get('gsheet_key_2024')).worksheet("Pro-LGBTQ Bills")
+wks2 = gc.open_by_key(os.environ.get('gsheet_key_2025')).worksheet("Pro-LGBTQ Bills")
 expected_headers2 = wks2.row_values(1)
 
 # loads worksheet into dataframe
 prev_gsheet2 = pd.DataFrame(wks2.get_all_records(expected_headers=expected_headers2))
 
 # open worksheet
-wks3 = gc.open_by_key(os.environ.get('gsheet_key_2023')).sheet1
+wks3 = gc.open_by_key(os.environ.get('gsheet_key_2025')).sheet1
 expected_headers3 = wks3.row_values(1)
 
 # loads worksheet into dataframe
 prev_gsheet3 = pd.DataFrame(wks3.get_all_records(expected_headers=expected_headers3))
 
 # open worksheet
-wks4 = gc.open_by_key(os.environ.get('gsheet_key_2023')).worksheet("Pro-LGBTQ Bills")
+wks4 = gc.open_by_key(os.environ.get('gsheet_key_2025')).worksheet("Pro-LGBTQ Bills")
 expected_headers4 = wks4.row_values(1)
 
 # loads worksheet into dataframe
@@ -147,6 +147,7 @@ expected_headers5 = wks5.row_values(1)
 
 # loads worksheet into dataframe
 prev_gsheet5 = pd.DataFrame(wks5.get_all_records(expected_headers=expected_headers5))
+
 
 ignore_list = pd.read_json("../cache/ignore_list.json")
 
